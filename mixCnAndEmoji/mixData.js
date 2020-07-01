efde7c4999a59f3effd9c855f0ffccf63f0050f2
brian <chiyu823@pingan.com.cn>
2020-07-01 15:10:52 +0800

    fix: emoji数据处理更改

--- mixCnAndEmoji/mixData.js
+++ mixCnAndEmoji/mixData.js
@@ -6,7 +6,16 @@ const fs = require('fs')
 const path = require('path')
 let nullData = []
 for(let k in twEmoji.emojis) {
-    let _bean = cnData[twEmoji.emojis[k].b]
+    let _unified = twEmoji.emojis[k].b
+    let _bean = cnData[_unified]
+
+    if (!_bean && _unified.endsWith("-FE0F")) {
+        let uniArr = _unified.split('-')
+        uniArr.pop()
+        _unified = uniArr.join('-')
+        _bean = cnData[_unified]
+    }
+    
     if (_bean) {
         !twEmoji.emojis[k].n && (twEmoji.emojis[k].n = [])
         twEmoji.emojis[k].n.push(_bean.name)
@@ -21,5 +30,9 @@ for(let k in twEmoji.emojis) {
 console.log(nullData)
 console.log('path==', path.join(__dirname,'./minCnAndEmoji/emoji.json'))
 fs.writeFile(path.join(__dirname,'./emoji.json'), JSON.stringify(twEmoji), (a,b) => {
+    console.log(a,b)
+})
+
+fs.writeFile(path.join(__dirname,'./nullemoji.json'), JSON.stringify(nullData), (a,b) => {
     console.log(a,b)
 })
